021a4fd8ea96ccdbe65f8db119434b5fe929294f
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-09-08 19:08:17 -0500

    linter configured

--- pages/views.py
+++ pages/views.py
@@ -18,46 +18,39 @@ from .models import Product, Review
 
 
 class HomePageView(TemplateView): 
-    template_name = "pages/home.html"
+  template_name = 'pages/home.html'
     
 
 class AboutPageView(TemplateView):
-  template_name = "pages/about.html"
+  template_name = 'pages/about.html'
   
   def get_context_data(self, **kwargs): 
-    context = super().get_context_data(**kwargs) 
+      context = super().get_context_data(**kwargs) 
 
-    context.update({ 
+      context.update({ 
+          'title': 'Tienda de Café', 
+          'subtitle': '¡Para los verdaderos amantes de Café !', 
+          'description': 'Tu café ideal, a un solo clic', 
+          'author': 'Developed by: Mario Alejandro Muñetón Durango', 
+        }) 
 
-      "title": "Tienda de Café", 
-
-      "subtitle": "¡Para los verdaderos amantes de Café !", 
-
-      "description": "Tu café ideal, a un solo clic", 
-
-      "author": "Developed by: Mario Alejandro Muñetón Durango", 
-
-    }) 
-
-
-
-    return context
+      return context
     
     
     
 class ProductIndexView(View): 
 
-  template_name = "products/index.html" 
+  template_name = 'products/index.html' 
 
   def get(self, request): 
 
     viewData = {} 
 
-    viewData["title"] = "Tienda de Café - El Barista" 
+    viewData['title'] = 'Tienda de Café - El Barista' 
 
-    viewData["subtitle"] =  "Productos - Métodos de Filtrado" 
+    viewData['subtitle'] =  'Productos - Métodos de Filtrado' 
 
-    viewData["products"] = Product.objects.all() 
+    viewData['products'] = Product.objects.all() 
 
 
 
@@ -66,7 +59,7 @@ class ProductIndexView(View):
  
 
 class ProductShowView(View): 
-  template_name = "products/show.html"
+  template_name = 'products/show.html'
   
   def get(self, request, id): 
     # Check if product id is valid 
@@ -74,36 +67,36 @@ class ProductShowView(View):
       product_id = int(id) 
 
       if product_id < 1: 
-        raise ValueError("Product id must be 1 or greater") 
+        raise ValueError('Product id must be 1 or greater') 
 
       product = get_object_or_404(Product, pk=product_id) 
 
     except (ValueError, IndexError): 
 
       # If the product id is not valid, redirect to the home page 
-      return HttpResponseRedirect(reverse("home")) 
+      return HttpResponseRedirect(reverse('home')) 
     
     viewData = {} 
 
     product = get_object_or_404(Product, pk=product_id) 
 
-    viewData["title"] = product.tittle + " - Tienda de Café - El barista" 
+    viewData['title'] = product.tittle + ' - Tienda de Café - El barista' 
 
-    viewData["subtitle"] =  product.tittle + " - Product information" 
+    viewData['subtitle'] =  product.tittle + ' - Product information' 
 
-    viewData["product"] = product 
+    viewData['product'] = product 
 
     return render(request, self.template_name, viewData)
     
 class ProductListView(ListView): 
   model = Product 
-  template_name = "product_list.html" 
-  context_object_name = "products"  # This will allow you to loop through 'products' in your template 
+  template_name = 'product_list.html' 
+  context_object_name = 'products'  # This will allow you to loop through 'products' in your template 
 
   def get_context_data(self, **kwargs): 
     context = super().get_context_data(**kwargs) 
-    context["title"] = "Tienda de Café - El Barista" 
-    context["subtitle"] = "List of products" 
+    context['title'] = 'Tienda de Café - El Barista' 
+    context['subtitle'] = 'List of products' 
 
     return context    
     
@@ -111,26 +104,26 @@ class ProductListView(ListView):
 class ProductForm(forms.ModelForm): 
   class Meta: 
     model = Product 
-    fields = "__all__" 
+    fields = '__all__' 
 
   def clean_price(self): 
-    price = self.cleaned_data.get("price") 
+    price = self.cleaned_data.get('price') 
 
     if price is not None and price <= 0: 
-      raise ValidationError("Price must be greater than zero.") 
+      raise ValidationError('Price must be greater than zero.') 
 
     return price 
 
 
 class ProductCreateView(View): 
-  template_name = "products/create.html" 
+  template_name = 'products/create.html' 
 
   def get(self, request): 
     form = ProductForm() 
 
     viewData = {} 
-    viewData["title"] = "Create product" 
-    viewData["form"] = form 
+    viewData['title'] = 'Create product' 
+    viewData['form'] = form 
 
     return render(request, self.template_name, viewData) 
 
@@ -138,21 +131,21 @@ class ProductCreateView(View):
     form = ProductForm(request.POST) 
 
     if form.is_valid(): 
-      messagebox.showinfo(message="Elemento creado satisfactoriamente")
+      messagebox.showinfo(message='Elemento creado satisfactoriamente')
       form.save() 
-      return redirect("index")  
+      return redirect('index')  
 
     else: 
       viewData = {} 
-      viewData["title"] = "Create product" 
-      viewData["form"] = form 
+      viewData['title'] = 'Create product' 
+      viewData['form'] = form 
 
       return render(request, self.template_name, viewData)
         
         
 class ProductDeleteView(View):
     
-    template_name = "products/delete.html"
+    template_name = 'products/delete.html'
     
     def get(self, request, id):
         # Check if product id is valid 
@@ -163,7 +156,7 @@ class ProductDeleteView(View):
 
             if product_id < 1: 
 
-                raise ValueError("Product id must be 1 or greater") 
+                raise ValueError('Product id must be 1 or greater') 
 
             product = get_object_or_404(Product, id=product_id) 
 
@@ -171,24 +164,24 @@ class ProductDeleteView(View):
 
             # If the product id is not valid, redirect to the home page 
 
-            return HttpResponseRedirect(reverse("home")) 
+            return HttpResponseRedirect(reverse('home')) 
          
         product = get_object_or_404(Product, id=product_id)
         product.delete()
-        return redirect("index")
+        return redirect('index')
     
 
 def detail(request, product_id):
     product = get_object_or_404(Product,pk=product_id)
     reviews = Review.objects.filter(product = product)
-    return render(request, "detail.html", 
-                  {"product":product, "reviews": reviews})
+    return render(request, 'detail.html', 
+                  {'product':product, 'reviews': reviews})
 
 @login_required    
 def createReview(request, product_id):
     product = get_object_or_404(Product,pk=product_id)
-    if request.method == "GET":
-        return render(request, "reviews/createReview.html", {"form":ReviewForm(), "product":product})
+    if request.method == 'GET':
+        return render(request, 'reviews/createReview.html', {'form':ReviewForm(), 'product':product})
     else:
         try:
             form = ReviewForm(request.POST)
@@ -196,34 +189,34 @@ def createReview(request, product_id):
             newReview.user = request.user
             newReview.product = product
             newReview.save()
-            return redirect("show", newReview.product.id)
+            return redirect('show', newReview.product.id)
         
         except ValueError:
-          return render(request,"reviews/createReview.html", {"form":ReviewForm(), "error":"bad data passed in"})
+          return render(request,'reviews/createReview.html', {'form':ReviewForm(), 'error':'bad data passed in'})
         
         
 @login_required
-def updatereview(request, review_id):
+def updateReview(request, review_id):
     review = get_object_or_404(Review,pk=review_id,user=request.user)
-    if request.method == "GET":
+    if request.method == 'GET':
         form = ReviewForm(instance=review)
-        return render(request, "updatereview.html", 
-                      {"review": review,"form":form})
+        return render(request, 'updateReview.html', 
+                      {'review': review,'form':form})
     else:
         try:
             form = ReviewForm(request.POST, instance=review)
             form.save()
-            return redirect("detail", id)
+            return redirect('detail', id)
         except ValueError:
-            return render(request, "updatereview.html",
-             {"review": review,"form":form,"error":"Bad data in form"})
+            return render(request, 'updateReview.html',
+             {'review': review,'form':form,'error':'Bad data in form'})
             
 
 @login_required
-def deletereview(request, review_id):
+def deleteReview(request, review_id):
     review = get_object_or_404(Review, pk=review_id, user=request.user)
     review.delete()
-    return redirect("detail", review.product.id)
+    return redirect('detail', review.product.id)
         
         
         
