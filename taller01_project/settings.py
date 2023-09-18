ed97af3c4c80f4439a6c2449aa681d40e55ee6a3
unknown <juanjosev2003@hotmail.es>
2023-09-18 11:27:11 -0500

    Host local 3

--- taller01_project/settings.py
+++ taller01_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-t%l*yjf!egfy^w9+43q8mm-=3vvg&%asb_#l_y0=t+$npt)&k-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.170.218.230', '127.0.0.1']
+ALLOWED_HOSTS = ['34.170.218.230']
 
 
 # Application definition
