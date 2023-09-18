7a0d7d1a249abab8e6bccf6250b97c23bddddbb0
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-09-18 18:22:37 -0500

    production and development environment created

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taller01_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taller01_project.settings_development' if os.environ.get('ENVIRONMENT') == 'development' else 'taller01_project.settings_production')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
